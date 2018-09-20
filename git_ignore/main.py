0f19f872cc04a486a42b1a50106721851b5b70d0
ghoostqq <qiu.gits@gmail.com>
2018-09-20 14:30:01 +0900

    Update: show helpful description

--- git_ignore/main.py
+++ git_ignore/main.py
@@ -35,11 +35,37 @@ class Template:
             fout.write(template.decode())
 
 
-@click.command()
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+
+@click.command(context_settings=CONTEXT_SETTINGS)
+@click.option('--show', '-s', is_flag=True, help='Show supported ARGS and exit.')
 @click.argument('args', nargs=-1)
 @click.version_option(version=__version__)
-def main(args):
+def main(show, args):
+    '''
+    Specify the language etc, whose template you want to use, in ARGS.
+    You can specify multiple ARGS at once.
+
+    Example usage:
+
+        $ git-ignore python sass
+
+        This will create .gitignore from both templates of Python ans Sass.
+
+    Show supported ARGS:
+
+        $ git-ignore
+
+        or
+
+        $ git-ignore --show
+    '''
     tem = Template()
+
+    if show or not args:
+        tem.print_available()
+        return
 
     nomatch = list()
     matched = list()
