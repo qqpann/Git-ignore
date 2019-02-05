e217b2877b8cbd2f87b724983a37da9a8c49d552
Qiushi Pan <qiu.gits@gmail.com>
2019-02-05 19:31:45 +0900

    Test: basic usage will create .gitignore if not exists

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,24 @@
+import os
 import unittest
 
+from click.testing import CliRunner
+
+from git_ignore.main import main as cli
 from git_ignore.main import Template
 
 
 class TemplateTests(unittest.TestCase):
     def setUp(self):
         self.template = Template()
+        self.runner = CliRunner()
 
     def test_template_dict_is_not_empty(self):
-        print(self.template.TEMPLATE_DICT)
         self.assertTrue(self.template.TEMPLATE_DICT)
+
+    def test_simplest_usage(self):
+        with self.runner.isolated_filesystem():
+            self.assertFalse('.gitignore' in os.listdir())
+
+            self.runner.invoke(cli, ['python'])
+
+            self.assertTrue('.gitignore' in os.listdir())
